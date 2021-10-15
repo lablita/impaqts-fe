1c3cf46fe9c0aae8d4ff530a5b540acc38eb0b4a
Marco Fabbri <marco.fabbri@drwolf.it>
2021-10-15 11:14:41 +0200

    :green_heart: setting wss for production websocket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   installation: 'uno',
   installationName: 'IMPAQTS1',
   installationUrl: 'https://impaqts.drwolf.it/impaqts-configurator',
-  queryServerProtocol: 'ws',
+  queryServerProtocol: 'wss',
   queryServerHost: 'impaqts.drwolf.it/impaqts-be',
   RECONNECT_INTERVAL: 1000 // ms
 };
