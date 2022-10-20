fb4f16249771ec8cc3462acb391e3a5fefdfc54e
Simone Guasti <simone.guasti@drwolf.it>
2022-10-20 10:33:23 +0200

    :hammer: selectedCorpus and simpleQuery added to local storage

--- src/app/queries-container/queries-container.component.ts
+++ src/app/queries-container/queries-container.component.ts
@@ -195,6 +195,8 @@ export class QueriesContainerComponent implements OnInit, AfterViewInit {
   }
 
   public makeConcordances(sortQueryRequest?: SortQueryRequest): void {
+    localStorage.setItem('selectedCorpus', JSON.stringify(this.selectedCorpus));
+    localStorage.setItem('simpleQuery', this.simple);
     if (!sortQueryRequest) {
       this.queryRequestService.resetOptionsRequest();
     }
@@ -278,6 +280,8 @@ export class QueriesContainerComponent implements OnInit, AfterViewInit {
 
   private init(): void {
     const inst = localStorage.getItem(INSTALLATION);
+    this.selectedCorpus = !!localStorage.getItem('selectedCorpus') ? JSON.parse(localStorage.getItem('selectedCorpus')!) : null;
+    this.simple = localStorage.getItem('simpleQuery')!;
     if (inst) {
       this.installation = JSON.parse(inst) as Installation;
       this.installation.corpora.forEach(corpus => this.corpusList.push(new KeyValueItem(corpus.name, corpus.name)));
