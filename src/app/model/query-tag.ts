d619b5f0d6c01a1de6b779b5efb96521e5ba990c
Marco Fabbri <marco.fabbri@drwolf.it>
2022-11-30 09:30:31 +0100

    :fire: removed STRUCT_DOC from metadata retrieval

--- src/app/model/query-tag.ts
+++ src/app/model/query-tag.ts
@@ -14,7 +14,11 @@ export class QueryTag {
 
   constructor(struct: string, name?: string, value?: string) {
     this.structure = struct === TOKEN ? TOKEN : STRUCT_DOC;
-    if (!!name) this.name = name;
-    if (!!value) this.value = value;
+    if (!!name) {
+      this.name = name;
+    }
+    if (!!value) {
+      this.value = value;
+    }
   }
 }
