9b5d31862abdcc2712514c3b6caf406ada168764
Simone Guasti <simone.guasti@drwolf.it>
2022-12-12 16:14:08 +0100

    :hammer: only selected fields are enabled for sorting

--- src/app/collocation-options-panel/collocation-options-panel.component.ts
+++ src/app/collocation-options-panel/collocation-options-panel.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { REQUEST_TYPE } from '../common/query-constants';
 import { CollocationOptionsQueryRequestDTO, DEFAULT_COLLOCATION_OPTIONS_QUERY_REQUEST } from '../model/collocation-options-query-request-dto';
 import { CollocationQueryRequest } from '../model/collocation-query-request';
-import { KeyValueItem } from '../model/key-value-item';
+import { KeyValueItemExtended } from '../model/key-value-item';
 import { QueryRequestService } from '../services/query-request.service';
 import { EmitterService } from '../utils/emitter.service';
 
@@ -20,7 +20,7 @@ const STAT_DESC: { [key: string]: string } = {
   LOG_DICE: 'd',
 };
 
-const OPTIOIN_LIST = [
+const OPTION_LIST = [
   'T_SCORE',
   'MI',
   'MI3',
@@ -28,6 +28,16 @@ const OPTIOIN_LIST = [
   'MIN',
   'LOG_DICE',
   'MI_LOG'
+];
+
+const OPTION_SORT_LIST = [
+  new KeyValueItemExtended('T_SCORE', 'T_SCORE', true),
+  new KeyValueItemExtended('MI', 'MI', true),
+  new KeyValueItemExtended('MI3', 'MI3', true), 
+  new KeyValueItemExtended('LOG', 'LOG', true),
+  new KeyValueItemExtended('MIN', 'MIN' , true),
+  new KeyValueItemExtended('LOG_DICE', 'LOG_DICE', true),
+  new KeyValueItemExtended('MI_LOG', 'MI_LOG', true)
 ];
 
 const ATTRIBUTE_LIST = [
@@ -48,15 +58,19 @@ export class CollocationOptionsPanelComponent {
 
   public collocationOptionsQueryRequest: CollocationOptionsQueryRequestDTO;
   public attributeList: string[] = ATTRIBUTE_LIST;
-  public optionList: string[] = OPTIOIN_LIST;
+  public optionList: string[] = OPTION_LIST;
+  public optionSortList: KeyValueItemExtended[] = OPTION_SORT_LIST;
 
   constructor(
     private readonly queryRequestService: QueryRequestService,
-    private readonly emitterService: EmitterService
+    private readonly emitterService: EmitterService,
+    //private readonly cd: ChangeDetectorRef
   ) {
     const collOptReq = localStorage.getItem(COLL_OPTIONS_QUERY_REQUEST);
+
     this.collocationOptionsQueryRequest = collOptReq ?
       JSON.parse(collOptReq) : DEFAULT_COLLOCATION_OPTIONS_QUERY_REQUEST;
+      this.changeSortListEnabled();
   }
 
   public closeSidebar(): void {
@@ -79,6 +93,19 @@ export class CollocationOptionsPanelComponent {
     this.closeSidebarEvent.emit(true);
   }
 
+  public changeSortListEnabled(): void {
+    let sortList: KeyValueItemExtended[] = [];
+    this.inactiveAllOptionSortList();
+    this.collocationOptionsQueryRequest.showFunc?.forEach(c => {
+      const optSort = this.optionSortList.find(o => o.key === c);
+      if (optSort) {
+        optSort.inactive = false
+      } 
+    });
+    sortList = this.optionSortList.slice();
+    this.optionSortList = sortList;
+  }
+
   private collocationQueryRequestBuild(collocationOptionsQueryRequest: CollocationOptionsQueryRequestDTO): CollocationQueryRequest {
     const res = new CollocationQueryRequest();
     res.attribute = !!collocationOptionsQueryRequest.attribute ? collocationOptionsQueryRequest.attribute : null;
@@ -91,6 +118,11 @@ export class CollocationOptionsPanelComponent {
     }
     res.sortBy = !!collocationOptionsQueryRequest.sortBy ? STAT_DESC[collocationOptionsQueryRequest.sortBy] : null;
     return res;
+  }
+
+  private inactiveAllOptionSortList(): void {
+    this.optionSortList.forEach(o => o.inactive = true);
+    this.collocationOptionsQueryRequest.sortBy = null;
   }
 
 }
