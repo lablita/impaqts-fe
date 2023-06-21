7d9e40d242eae49037e1332fd193bbc1ea91cc6c
Simone Guasti <simone.guasti@drwolf.it>
2023-06-21 17:35:23 +0200

    :construction:

---
+++ src/app/services/export-csv.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { HTTP, HTTPS } from '../common/constants';
+import { EXPORT_CSV } from '../common/routes-constants';
+import { EXPORT_FAILED, INSTALLATION } from '../model/constants';
+import { Installation } from '../model/installation';
+import { QueryRequest } from '../model/query-request';
+import { UtilService } from '../utils/util.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExportCsvService {
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly utils: UtilService
+    ) { }
+
+  public exportCvs(request: QueryRequest, type: string): Observable<string> {
+    const inst = localStorage.getItem(INSTALLATION);
+    if (inst) {
+      const installation = JSON.parse(inst) as Installation;
+      const endpoint = installation?.corpora.find(corp => corp.name === request.corpus)?.endpoint;
+      const url = (environment.secureUrl ? HTTPS : HTTP) + endpoint;
+      return this.http.post<string>(`${url}/${EXPORT_CSV}/${type}`, request)
+      .pipe(catchError(this.utils.handleErrorObservable('exportCvs', EXPORT_FAILED, '')));
+    } else {
+      return of('');
+    }
+  }
+}
